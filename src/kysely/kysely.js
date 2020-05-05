1f2326f2d3d74f96244f89734453d120684fbfb4
okramnesseam <bgh264@myy.haaga-helia.fi>
2020-05-05 14:41:43 +0300

    test

--- src/kysely/kysely.js
+++ src/kysely/kysely.js
@@ -17,6 +17,7 @@ function Kyselyt() {
     let { id } = useParams();
     const [kyselyt, setKyselyt] = React.useState();
     const [virhe, setVirhe] = React.useState('Haetaan...');
+    const [open, setOpen] = useState(false);
 
     const fetchUrl = async () => {
         try {
@@ -27,18 +28,18 @@ function Kyselyt() {
             console.log(json);
             console.log(kyselyt);
 
-        } catch(error) {
+        } catch (error) {
             setVirhe('Nyt meni jokin väärin.');
-       }
+        }
     }
 
 
-    React.useEffect( () => { fetchUrl() }, []);
+    React.useEffect(() => { fetchUrl() }, []);
 
     if (virhe.length > 0) {
-        return(
+        return (
             <Grid container spacing={3} alignItems="center" justify="center">
-                <p>{ virhe }</p>
+                <p>{virhe}</p>
             </Grid>
         );
     }
@@ -46,48 +47,50 @@ function Kyselyt() {
     return (
         <div>
             {
-                        <Grid container spacing={3} alignItems="center" justify="center">
-                            <Grid item xs={4}>
-                                <ExpansionPanel>
-                                    <ExpansionPanelSummary
-                                        expandIcon={<ExpandMoreIcon />}
-                                        aria-controls="panel1a-content"
-                                        id="panel1a-header">
+                <Grid container spacing={3} alignItems="center" justify="center">
+                    <Grid item xs={4}>
+                        <ExpansionPanel>
+                            <ExpansionPanelSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header">
+                                <Typography>
+                                    {kyselyt.title}
+                                </Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <Button aria-label="answer" variant="contained" color="primary"
+                                    component={Link} to={'/vastaukset/' + id}>
+                                    <Typography>Näytä vastaukset</Typography>
+                                    {setOpen(true)};
+                                </Button>
+                                <Vastaukset />
+                                <Grid container alignItems="center" justify="center">
+                                    <Grid item xs={11}>
                                         <Typography>
-                                            {kyselyt.title}
+                                            Id: {kyselyt.queryId} <br />
+                                                Date: {kyselyt.date.substr(0, 10)}
                                         </Typography>
-                                    </ExpansionPanelSummary>
-                                    <ExpansionPanelDetails>
-                                    <Button aria-label="answer" variant="contained" color="primary"
-                                            component={Link} to ={'/vastaukset/' + id}>
-                                        <Typography>Näytä vastaukset</Typography>            
-                                    </Button>
-                                        <Grid container alignItems="center" justify="center">
-                                        <Grid item xs={11}>
-                                            <Typography>
-                                                Id: { kyselyt.queryId } <br/>
-                                                Date: { kyselyt.date.substr(0,10) }
-                                            </Typography>
-                                        </Grid>
-                                        </Grid>
-                                    </ExpansionPanelDetails>
-                                </ExpansionPanel>
-                                {/*tähän näin kysymys mappi*/}
-                                {
-                                    kyselyt.questions.map(kysymykset=>{
-                                        return(
-                                            <div style={{margin:20}}>
-                                            <Button aria-label="answer" variant="contained" color="primary"
-                                            component={Link} to ={'/vastaa/' + kysymykset.questionId}>
-                                                <Typography>{kysymykset.questionString}</Typography>
-                                                <QuestionAnswerIcon style={{ color: '#FFFFFF' }}/>
-                                            </Button>
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </Grid>
-                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                        {/*tähän näin kysymys mappi*/}
+                        {
+                            kyselyt.questions.map(kysymykset => {
+                                return (
+                                    <div style={{ margin: 20 }}>
+                                        <Button aria-label="answer" variant="contained" color="primary"
+                                            component={Link} to={'/vastaa/' + kysymykset.questionId}>
+                                            <Typography>{kysymykset.questionString}</Typography>
+                                            <QuestionAnswerIcon style={{ color: '#FFFFFF' }} />
+                                        </Button>
+                                    </div>
+                                )
+                            })
+                        }
+                    </Grid>
+                </Grid>
             }
         </div>
     );
